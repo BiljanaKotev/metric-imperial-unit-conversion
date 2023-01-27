c1f1d1dded6eeb1a0224c2df038f4962b88508e1
biljana_kotev <biljanakotev@gmail.com>
2023-01-27 10:41:31 +0100

    added id's to the p elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-//  1 meter/3.281 feet
-//  1 liters / 0.264 gallons
-//  1 kilograms / 2.204 pounds
+"use strict";
+
+const meterToFeet = 3.281;
+const feetToMeter = 0.304;
+const literToGallon = 0.264;
+const gallonToLiter = 3.785;
+const kiloToPound = 2.204;
+const poundToKilo = 0.453;
 
 let inputUnit = document.querySelector("#input-unit");
 let convertBtn = document.querySelector("#convert-btn");
+let lengthMeasurements = document.querySelector("#length-measurements");
 
 convertBtn.addEventListener("click", function () {
-  alert("hi");
+  let inputValue = inputUnit.value;
+  lengthMeasurements.innerHTML = `${inputValue} meters = ${
+    inputValue * meterToFeet
+  } feet | ${inputValue} feet = ${inputValue * feetToMeter} meters`;
 });
