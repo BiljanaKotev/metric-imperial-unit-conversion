8ca6b786586764cddbc73528ba18c2e3abb7c48c
biljana_kotev <biljanakotev@gmail.com>
2023-01-27 09:46:20 +0100

    updated input id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //  1 liters / 0.264 gallons
 //  1 kilograms / 2.204 pounds
 
-let unit = document.querySelector("#unit");
+let inputUnit = document.querySelector("#input-unit");
 let convertBtn = document.querySelector("#convert-btn");
 
 convertBtn.addEventListener("click", function () {
