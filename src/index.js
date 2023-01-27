f1f2c2c82ad85acc76d5d072bf9d3e7ced6f43e3
biljana_kotev <biljanakotev@gmail.com>
2023-01-27 12:15:53 +0100

    updated margin and padding on content container

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 "use strict";
 
-const meterToFeet = 3.281;
-const feetToMeter = 0.304;
-const literToGallon = 0.264;
-const gallonToLiter = 3.785;
-const kiloToPound = 2.204;
-const poundToKilo = 0.453;
+const meterToFeet = (3.281).toFixed(2);
+const feetToMeter = (0.3048).toFixed(2);
+const literToGallon = (0.264).toFixed(2);
+const gallonToLiter = (3.785).toFixed(2);
+const kiloToPound = (2.205).toFixed(2);
+const poundToKilo = (0.454).toFixed(2);
 
 let inputUnit = document.querySelector("#input-unit");
-let convertBtn = document.querySelector("#convert-btn");
-let lengthMeasurements = document.querySelector("#length-measurements");
+const convertBtn = document.querySelector("#convert-btn");
+const lengthMeasurement = document.querySelector("#length-measurement");
+const volumeMeasurement = document.querySelector("#volume-measurement");
+const massMeasurement = document.querySelector("#mass-measurement");
 
 convertBtn.addEventListener("click", function () {
   let inputValue = inputUnit.value;
-  lengthMeasurements.innerHTML = `${inputValue} meters = ${
+
+  lengthMeasurement.innerHTML = `${inputValue} meters = ${
     inputValue * meterToFeet
   } feet | ${inputValue} feet = ${inputValue * feetToMeter} meters`;
+
+  volumeMeasurement.innerHTML = `${inputValue} liters = ${
+    inputValue * literToGallon
+  } gallons | ${inputValue} gallons = ${inputValue * gallonToLiter} liters`;
+
+  massMeasurement.innerHTML = `${inputValue} kilos = ${
+    inputValue * kiloToPound
+  } pounds | ${inputValue} pounds = ${inputValue * poundToKilo} kilos`;
 });
